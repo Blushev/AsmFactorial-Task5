3626f179e65fc088999cba461c224f7081faa9d2
RUMYANCEV <egor.rumyancev.9494@mail.ru>
2021-05-20 12:42:41 +0500

    COMMIT

---
+++ Task5/Task5.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+		int x = 5;
+		int f = 0;
+		__asm {
+			mov eax, x
+			push eax
+			call fact
+			pop eax
+			mov f, eax
+			jmp l_end
+		fact:
+			push esi
+				mov esi, esp
+				add esi, 8
+				push edx
+				mov eax, [esi]
+				or eax, eax
+				jnz fact1
+				inc eax
+				jmp fact_end
+		fact1:
+			cmp eax, 1
+				je fact_end
+				mov edx, eax
+				dec eax
+				push eax
+				call fact
+				pop eax
+				mul edx
+		fact_end:
+			mov[esi], eax
+				pop edx
+				pop esi
+				ret
+		l_end:
+		}
+		cout << f << endl;
+	}
+
+
